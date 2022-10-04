a58330a534e775b8531e35ba0981451e443758f4
tessanyyy <tessanykeintjem@gmail.com>
2022-10-04 15:03:57 +0800

    First commit

--- src/com/juaracoding/gitjava/Main.java
+++ src/com/juaracoding/gitjava/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         //GitHub
         System.out.println("Hello world!");
+        System.out.println("Ini tambahan coding");
     }
 }
